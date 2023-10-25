8b02294bfbffdc89cfea300beecec8b286713daf
Lassus01 <146187747+Lassus01@users.noreply.github.com>
2023-10-25 16:17:34 +0300

    Add files via upload

---
+++ uno/test/test_add.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return x + y
+
+if __name__ == '__main__':
+    assert  5 == add(2, 3)
+    # assert 2 == 5, 'Проверяем сложение четного и нечетного'
+    # assert 7 == 7
+    # assert 7 == 13
